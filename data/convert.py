261669d6eb29d852b6ff8b7113a124c1736ec00c
xtudbxk <xtudbxk@126.com>
2020-05-18 22:12:50 +0800

    update code for cast colored images in PASCAL VOC to indexed images

--- data/convert.py
+++ data/convert.py
@@ -1,8 +1,24 @@
 import os
 import glob
 import shutil
+import numpy as np
 from scipy import io as scipy_io
 from skimage import io as skimage_io
+
+def rgb2label(label, colors=[], ignore_color=255):
+    if len(colors) <= 0:
+        colors = [(0, 0, 0), (128, 0, 0), (0, 128,0 ), (128, 128, 0),
+                  (0, 0, 128), (128, 0, 128), (0, 128, 128), (128, 128, 128),
+                  (64, 0, 0), (192, 0, 0), (64, 128, 0), (192, 128, 0),
+                  (64, 0, 128), (192, 0, 128), (64, 128, 128), (192, 128, 128),
+                  (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0),
+                  (0, 64, 128)] # using palette for pascal voc
+    rgb = ignore_color*np.ones(label.shape[0:2],dtype=np.uint8)
+    for i,c in enumerate(colors):
+        masks = label[:,:,0:3] == c
+        mask = np.logical_and(masks[:,:,2],np.logical_and(masks[:,:,0],masks[:,:,1]))
+        rgb[mask] = i
+    return rgb.astype(np.uint8)
 
 # paths
 sbd_path = os.path.join("benchmark_RELEASE","dataset","cls")
@@ -17,7 +33,12 @@ if os.path.exists(aug_path) is not True:
 pascal_filenames = glob.glob(os.path.join(pascal_path,"*.png"))
 for filename_index,single_pascal_filename in enumerate(pascal_filenames):
     if filename_index % 500 == 0: print(f"processing pascal dataset - total:{len(pascal_filenames)},now:{filename_index},{filename_index/len(pascal_filenames)*100:.3}%")
-    shutil.copy(single_pascal_filename,aug_path)
+    _,single_image_name = os.path.split(single_pascal_filename)
+    single_image = skimage_io.imread(single_pascal_filename)
+    if len(single_image.shape) >= 3 and single_image.shape[2] > 1:
+        single_image = single_image[:,:,:3]
+        single_image = rgb2label(single_image)
+    skimage_io.imsave(os.path.join(aug_path,single_image_name),single_image)
 
 # moving data from sbd to aug
 sbd_filenames = glob.glob(os.path.join(sbd_path,"*.mat"))
